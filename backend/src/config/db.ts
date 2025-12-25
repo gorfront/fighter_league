5394510d24dc95842c174eebbc4656d07e2688cc
akselpapoyan1 <akselpapoyan123@gmail.com>
2025-12-25 18:51:49 +0400

    fix db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,3 +1,23 @@
+// import pkg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: (process.env.DB_PORT || 5432) as number,
+//   max: 10,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// });
+
+// export default pool;
+
 import pkg from "pg";
 import dotenv from "dotenv";
 
@@ -5,15 +25,27 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
+  connectionString: connectionString ? connectionString : undefined,
+
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: (process.env.DB_PORT || 5432) as number,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : undefined,
 });
 
 export default pool;
