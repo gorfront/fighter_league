94cd6d009a7e1655841bc2496c3ee111b27a4c35
gorfront <avakimyan.gor.00@gmail.com>
2025-12-27 23:17:38 +0400

    create new email pass and change port

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -13,15 +13,12 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  logger: true,
-  debug: true,
   connectionTimeout: 40000,
 });
 
